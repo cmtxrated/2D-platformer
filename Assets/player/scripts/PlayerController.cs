49ebadb507c319ec567218a717574d28b9171674
cmtxrated <43762237+cmtxrated@users.noreply.github.com>
2018-11-13 15:50:14 +0000

    Add files via upload

---
+++ Assets/player/scripts/PlayerController.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+
+    public float speed;
+    public float jumpForce;
+    private float moveInput;
+    private Rigidbody2D rb;
+
+    private bool facingRight = true;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    void FixedUpdate()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal");
+        rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
+
+        
+
+
+
+
+       
+ 
+    }
+
+
+
+    void Flip()
+    {
+        facingRight = !facingRight;
+        Vector3 Scaler = transform.localScale;
+        Scaler.x *= -1;
+        transform.localScale = Scaler;
+
+    }
+}
