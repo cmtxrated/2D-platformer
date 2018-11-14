471ddfa32f50daefd5e4702407e91ef2562e3953
cmtxrated <43762237+cmtxrated@users.noreply.github.com>
2018-11-14 15:31:06 +0000

    Add files via upload

--- Assets/player/scripts/PlayerController.cs
+++ Assets/player/scripts/PlayerController.cs
@@ -10,8 +10,19 @@ public class PlayerController : MonoBehaviour
     public float jumpForce;
     private float moveInput;
     private Rigidbody2D rb;
+    private bool isJumping = false;
+    public Transform groundCheck;
 
     private bool facingRight = true;
+    public bool grounded;
+
+
+    private void Jump()
+    {
+        rb.AddForce(new Vector2(0f, jumpForce));
+        grounded = false;
+        isJumping = true;
+    }
 
     void Start()
     {
@@ -24,17 +35,17 @@ public class PlayerController : MonoBehaviour
         moveInput = Input.GetAxisRaw("Horizontal");
         rb.velocity = new Vector2(moveInput * speed, rb.velocity.y);
 
-        
 
+        if (Input.GetKeyDown(KeyCode.W) && grounded)
+        {
+            Jump();
 
-
-
+        }
        
- 
     }
 
 
-
+  
     void Flip()
     {
         facingRight = !facingRight;
@@ -42,5 +53,13 @@ public class PlayerController : MonoBehaviour
         Scaler.x *= -1;
         transform.localScale = Scaler;
 
+    }
+
+    public void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Platform")
+        {
+            grounded = true;
+        }
     }
 }
